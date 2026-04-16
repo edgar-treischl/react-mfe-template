052616ad923e6e71eb5fa00a48d461f190ffece5
edgar.treischl <edgar.treischl@isb.bayern.de>
2026-04-16 20:25:13 +0200

    test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
-// https://vite.dev/config/
+const sharedModules: Record<string, { singleton: boolean }> = {
+  react: { singleton: true },
+  'react-dom': { singleton: true },
+};
+
 export default defineConfig({
-  plugins: [react()],
-})
+  // IMPORTANT for GitHub Pages deployment
+  base: '/app3/',
+
+  plugins: [
+    react(),
+    federation({
+      name: 'app3',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './HRApp': './src/App.tsx',
+      },
+
+      shared: sharedModules,
+    }),
+  ],
+
+  server: {
+    host: true,
+    port: 5176,
+    cors: true,
+  },
+
+  preview: {
+    host: true,
+    port: 5176,
+  },
+
+  build: {
+    target: 'esnext',
+    cssCodeSplit: false,
+    minify: false,
+  },
+});
